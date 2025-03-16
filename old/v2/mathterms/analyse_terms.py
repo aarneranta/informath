dd16186921ff5e00fbd8e5c3b99aab071e608413
Aarne Ranta <aarne@chalmers.se>
2025-03-16 12:17:58 +0100

    generating GF functions and links to Wikidata Qids for filtered derived terms

--- old/v2/mathterms/analyse_terms.py
+++ old/v2/mathterms/analyse_terms.py
@@ -32,12 +32,40 @@ gfuns = [(g[0][0], [f[1] for f in g]) for g in gfuns]
 clean_gfuns = [f for f in gfuns if clean_word(f[0][0]) and clean_word(f[0][1])]
 
 # to create data for manual inspection: redirect to terms.tmp and remove unwanted lines
-for fun in clean_gfuns:
-    print(fun)
+if __name__ == '__main1__':
+    for fun in clean_gfuns:
+        print(fun)
 
 # to test data integrity
-"""
-with open('terms.tmp') as file:
-    for line in file:
-        print(type(eval(line)))
-"""        
+if __name__ == '__main2__':
+    with open('terms.tmp') as file:
+        for line in file:
+            print(type(eval(line)))
+
+def mk_fun(s):
+    s = '_'.join(s.split())  # spaces replaced by underscores
+    
+    if (s[0].isalpha() and
+        all(ord(c)<256 and (c.isdigit() or c.isalpha() or c in "_'")
+            for c in s)):  # test if legal GF identifier
+        return s
+    else:
+        return "'" + s.replace("'", "\\'") + "'"  # if not, single quotes make it legal
+            
+def gf_abs_rule(t):
+    cat = t[1][0][-2:]
+    ids = [t.split('_')[-3] for t in t[1]]
+    fun = mk_fun(' '.join([w for w in t[0][:-1] if w != 'None'] + [cat]))
+    return f'fun {fun} : {cat} ; -- {", ".join(ids)}'
+
+
+# to generate GF
+if __name__ == '__main__':
+    with open('filtered-terms.tmp') as file:
+        for line in file:
+            print(gf_abs_rule(eval(line)))
+
+
+
+        
+
